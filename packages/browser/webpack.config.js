98b45de96c1a5bebc12873a898b1eb4108b3e48e
Pedro Gomes <pedrogomes94@gmail.com>
2019-01-07 23:11:38 +0000

    webpack refactor

---
+++ packages/browser/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const pkg = require('./package.json')
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    index: './src/index.ts',
+    'index.min': './src/index.ts'
+  },
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: '[name].js',
+    libraryTarget: 'umd',
+    library: pkg.name,
+    umdNamedDefine: true
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  devtool: 'source-map',
+  optimization: {
+    minimize: true
+  },
+  module: {
+    rules: [{ test: /\.tsx?$/, loader: 'ts-loader' }]
+  }
+}
